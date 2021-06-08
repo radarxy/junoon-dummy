7ce9cb7e13f0eafa765ecb70596923c355c2b8f4
Kaku-g <rjjatinrawat9@gmail.com>
2021-06-08 15:20:45 +0530

    login and signup page added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import Login from './Login/Login';
+import './Login/Login.css';
+import Hero from './Hero/Hero';
 
-function App() {
+import './App.css';
+import fire from './fireBase';
+
+const App =() => {
+
+  const [user, setUser] = useState('');
+  const [email,setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError,setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [hasAccount,setHasAccount] = useState(false);
+
+  const clearInputs = () => {
+    setEmail('');
+    setPassword('');
+  }
+
+  const clearErrors = () => {
+    setEmailError('');
+    setPasswordError('');
+  }
+  
+  const handleLogin = () => {
+    clearErrors();
+    fire.auth().signInWithEmailAndPassword(email,password)
+    .catch((err)  =>{
+      switch (err.code) {
+        case "auth/invalid-email":
+          case "auth/user-disabled":
+            case "auth/user-not-found":
+               setEmailError(err.message);
+               break;
+               case "auth/wrong-password":
+                 setPasswordError(err.message);
+                 break;
+      }
+    });
+  };
+
+    const handleSignUp = () => {
+      clearErrors();
+      fire.auth()
+      .createUserWithEmailAndPassword(email,password)
+      .catch((err) => {
+        switch(err.code){
+          case "auth/email-already-in-use":
+            case "auth/invalid-email":
+              setEmailError(err.message);
+              break;
+              case "auth/weak-password":
+                setPasswordError(err.message);
+                break;
+        }
+      });
+    };
+
+    const handleLogout = () =>{
+
+      fire.auth().signOut();
+    };
+    
+    const authListener = () => {
+      fire.auth().onAuthStateChanged((user) => {
+        if(user) {
+          clearInputs();
+          setUser(user);
+        }
+          else  {
+            setUser("");
+          }
+        
+      });
+    };
+
+    
+    useEffect(() => {
+      authListener();
+    }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Kyu Kya hua, nope,kuvh ni
-        </a>
-      </header>
+   
+   <div className="App">
+      {user?
+      (
+  <Hero handleLogout={handleLogout}/>
+  
+      ):
+      (
+        <Login email ={email}
+        setEmail={setEmail}
+        password={password}
+         setPassword={setPassword}
+          handleLogin={handleLogin} 
+          handleSignUp={handleSignUp} 
+          hasAccount={hasAccount} 
+          setHasAccount={setHasAccount} 
+          emailError={emailError}
+          passwordError={passwordError}/>
+      )};
+      
+         
     </div>
   );
 }
